3c1139757bbfedbe15afdd55260d8cbe1ba33c8f
viraltri19 <viraltrivedi93@gmail.com>
2025-06-04 14:20:58 -0400

    Remove wrong file

--- 03_Database.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(host= "localhost", user= "viral", passwd= "Vir@24")
-mycursor= mydb.cursor()
-
-mycursor.execute("show Databases")
-for i in mycursor:
-    print(i)
-
