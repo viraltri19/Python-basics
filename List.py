0c4bf2ec21cdd4bc5dcff9476891d7f4860e1700
viraltri19 <viraltrivedi93@gmail.com>
2025-06-04 18:58:45 -0400

    All basic concepts of Python

---
+++ List.py
@@ -0,0 +1,17 @@
+# append : To add value in the end of the list
+list = ['Karan',34,89,9,78,"Helo"]
+list.append(7)
+print(list)
+
+list1 = [34,78,90,56]
+
+list1.sort()
+print(list1)
+
+list1.reverse()
+list1.sort(reverse=True)
+print(list1)
+
+list1.insert(2,'j')
+print(list1)
+
